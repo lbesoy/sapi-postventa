c01c07040d5503f73e1e5fa134a1b1475ac6da08
Pablo Besoy <pablobesoy@gmail.com>
2026-05-26 19:42:50 -0600

    chore: clean up repository root, removing obsolete test files, JSONs, and old backup files

--- test_api.js
+++
@@ -1,5 +0,0 @@
-const fetch = require('node-fetch'); // Let's see if we can use native fetch or node-fetch
-fetch('https://eurorep-api.onrender.com/api/clientes?queryCode=eurorep_clientes')
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data.slice(0, 2), null, 2)))
-  .catch(console.error);
--- test_sap.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-async function test() {
-  const url = 'https://eurorep-api.onrender.com/api/sap/queries/SELECT%20T0.%22Memo%22%20as%20ID_SLP%2C%20T0.%22SlpCode%22%20as%20ID_SAP%2C%20T0.%22SlpName%22%20as%20Nombre%2C%20T0.%22Fax%22%20as%20Tipo_Usuario%2C%20%20T0.%22Mobil%22%20as%20Celular%20FROM%20OSLP%20T0/execute';
-  // I don't have direct access to the Render API DB.
-}
